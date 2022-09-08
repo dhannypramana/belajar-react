1606a8af53385c72979ca22d4692255195bc87c9
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-09-08 13:08:30 +0700

    Data Fetching di React

--- src/components/DataFetching/index.jsx
+++ src/components/DataFetching/index.jsx
@@ -1,14 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const spaceflightnewsapi = 'https://api.spaceflightnewsapi.net/v3/blogs';
 const dobhaApi = 'https://dobha.herokuapp.com/api/read-all-article';
 
 const DataFetch = () => {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
     const getData = async () => {
       const request = await fetch(spaceflightnewsapi);
       const response = await request.json();
       console.log(response);
+      setTimeout(() => {
+        setNews(response);
+        setLoading(false);
+      }, 2000);
     };
 
     getData();
@@ -17,6 +24,13 @@ const DataFetch = () => {
   return (
     <>
       <h1>Data Fetching</h1>
+      {loading ? (<i>Loading Data....</i>) 
+        : (
+        <ul>
+          {news.map(item => {
+              return <li key={item.id}>{item.title}</li>
+            })}
+        </ul>)}
     </>
   );
 };
