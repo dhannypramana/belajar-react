eaa6f5e01998bd25cbf1aca05b589351d6edaa62
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2022-09-08 15:36:54 +0700

    Todo App With Delete Function

--- src/components/TodoApp/index.jsx
+++ src/components/TodoApp/index.jsx
@@ -5,26 +5,9 @@ const TodoApp = () => {
   const [activity, setActivity] = useState('');
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [darkMode, setDarkMode]= useState(false);
 
-  const title = useRef('');
-  const list = useRef('');
-  const label = useRef('');
-
-  const modeHandler = () => {
-    if (darkMode) {
-      setDarkMode(false);
-      document.body.style.backgroundColor = "white";
-      title.current.style.color = "black";
-      list.current.style.color = "black";
-      label.current.style.color = "black";
-    } else {
-      setDarkMode(true);
-      document.body.style.backgroundColor = "black";
-      title.current.style.color = "white";
-      list.current.style.color = "white";
-      label.current.style.color = "white";
-    }
+  const generateID = () => {
+    return Date.now();
   };
 
   const todoHandler = e => {
@@ -33,30 +16,43 @@ const TodoApp = () => {
     if (activity.length === 0) {
       return;
     } else {
-      setTodos([...todos, activity]);
+      setTodos([...todos, {
+        id: generateID(),
+        activity,
+      }]);
     }
     setActivity('');
+  };
+
+  const deleteTodoHandler = todoID => {
+    const filtered = todos.filter(todo => {
+      return todo.id !== todoID
+    });
+
+    setTodos(filtered);
   };
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1000);
   }, [loading]);
 
   return (
     <>
-      <button onClick={modeHandler}>{darkMode ? ('Light Mode') : ('Dark Mode')}</button>
-      <h1 ref={title}>Todo App</h1>
+      <h1>Todo App</h1>
       <form onSubmit={todoHandler}>
-        <label ref={label}>Activity</label><br />
+        <label>Activity</label><br />
         <input type="text" value={activity} onChange={e => setActivity(e.target.value)} placeholder="Add New Activity" className="input-box" />
         <button className="btn">Add Activity</button>
       </form>
         <div className="list">
-          <ul ref={list}>
+          <ul>
             {loading ? (<i>Please Wait...</i>) : 
-              todos.map(item => <li key={item}>{item}</li>)}
+              todos.map(item =>
+                <li key={item.id}>{item.activity}
+                  <button onClick={deleteTodoHandler.bind(this, item.id)}>Delete</button>
+                </li>)}
           </ul>
         </div>
     </>
